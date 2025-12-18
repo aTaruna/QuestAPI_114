e381f495b4c2bb932c3da561c53def85d4a3be2c
aTaruna <aTaruna@users.noreply.github.com>
2025-12-18 12:31:43 +0700

    tambah function loadSiswa

--- app/src/main/java/com/example/act8_localrestapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/act8_localrestapi/viewmodel/HomeViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.act8_localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.act8_localrestapi.modeldata.DataSiswa
 import com.example.act8_localrestapi.repositori.RepositoriDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed interface StatusUiSiswa {
     data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
@@ -15,5 +20,16 @@ class HomeViewModel(private val repositoriDataSiswa : RepositoriDataSiswa) : Vie
         private set
     init{
         loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(repositoriDataSiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            }
+        }
     }
 }
