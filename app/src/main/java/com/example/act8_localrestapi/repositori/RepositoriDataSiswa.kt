38de4dc6f264689e97bba4f567c74a6f6e93685e
aTaruna <aTaruna@users.noreply.github.com>
2025-12-25 21:30:58 +0700

    memperbarui class dan interface

--- app/src/main/java/com/example/act8_localrestapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/act8_localrestapi/repositori/RepositoriDataSiswa.kt
@@ -6,14 +6,17 @@ import com.example.act8_localrestapi.modeldata.DataSiswa
 interface RepositoriDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
-    //suspend fun getSatuSiswa(id: Int,dataSiswa: DataSiswa)
-    //suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
-    //suspend fun deleteSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void>
 
     class JaringanRepositoryDataSiswa(
         private val serviceApiSiswa: ServiceApiSiswa
     ):RepositoriDataSiswa{
         override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
         override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+        override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
+        override suspend fun editSatuSiswa(id: Int,dataSiswa: DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.editSatuSiswa(id,dataSiswa)
+        override suspend fun hapusSatuSiswa(id: Int):retrofit2.Response<Void> = serviceApiSiswa.hapusSatuSiswa(id)
     }
 }
